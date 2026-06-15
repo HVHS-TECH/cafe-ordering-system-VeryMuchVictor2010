7a4f4a4ae4574241a010774ef4c7e2c1bcf65f88
VeryMuchVictor2010 <24511vh@hvhs.school.nz>
2026-06-15 23:52:44 +0000

    Jun 15, 2026, 11:52 PM

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
 console.log("Hello world!")
-console.log{
-    "Running test"
-    PageRevealEvent.log
-    screenTop.PageRevealEvent
-    menubar
-
-}
-
-crossOriginIsolated{
-    console.log{"RBS"}
-    Image{Image.png}
-}
